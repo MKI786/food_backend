d834c943dbf6ff5a00f08791fd711a30c065ca42
MKI786 <136229720+MKI786@users.noreply.github.com>
2024-12-13 08:37:42 +0500

    Delete Middleware directory

--- Middleware/Nuetralmiddleware.js
+++
@@ -1,9 +0,0 @@
-const Nuetralmiddleware = async (req, res, next) => {
-    if (req.user && (req.user.role === "customer admin" || req.user.role === "supplier admin")) {
-        next();
-    } else {
-        res.status(403).json({ message: 'Access denied, admin only' });
-    }
-};
-
-module.exports = { Nuetralmiddleware };
--- Middleware/Sadminmiddleware.js
+++
@@ -1,9 +0,0 @@
-const Sadminmiddleware = async (req, res, next) => {
-    if (req.user && req.user.role === "supplier admin") {
-        next();
-    } else {
-        res.status(403).json({ message: 'Access denied, admin only' });
-    }
-};
-
-module.exports = { Sadminmiddleware };
